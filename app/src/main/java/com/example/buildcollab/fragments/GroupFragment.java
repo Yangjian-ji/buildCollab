67a10c94ff7663cfae3bef53c1db509226ce9513
Yang <y.ji@campus.fct.unl.pt>
2021-12-06 23:49:29 +0000

    Layout de profile feito sem testar

--- app/src/main/java/com/example/buildcollab/fragments/GroupFragment.java
+++ app/src/main/java/com/example/buildcollab/fragments/GroupFragment.java
@@ -1,22 +1,32 @@
 package com.example.buildcollab.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.buildcollab.R;
+import com.example.buildcollab.activity.NewGroupActivity;
 
 public class GroupFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View InputFragmentView = inflater.inflate(R.layout.fragment_group, container, false);
-
+        Button button = (Button) InputFragmentView.findViewById(R.id.newgroup);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), NewGroupActivity.class);
+                startActivity(intent);
+            }
+        });
         return InputFragmentView;
     }
 }
