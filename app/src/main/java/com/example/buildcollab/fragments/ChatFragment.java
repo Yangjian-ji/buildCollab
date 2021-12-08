6d55aff0a90cce0da076d3d6f44f0fc60a208e43
Yang <y.ji@campus.fct.unl.pt>
2021-12-08 17:09:36 +0000

    Adapters postos, só falta função dos proprios adapters

--- app/src/main/java/com/example/buildcollab/fragments/ChatFragment.java
+++ app/src/main/java/com/example/buildcollab/fragments/ChatFragment.java
@@ -8,14 +8,32 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.buildcollab.R;
 
 public class ChatFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View InputFragmentView = inflater.inflate(R.layout.fragment_chat, container, false);
+
+        mRecyclerView = InputFragmentView.findViewById(R.id.reclycleview);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(InputFragmentView.getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+
+        //Falta a leitura dos dados
+        //   mAdapter = new MyChatAdapter(LoadData);
+        mRecyclerView.setAdapter(mAdapter);
+
         return InputFragmentView;
     }
 }
--- app/src/main/java/com/example/buildcollab/fragments/GroupFragment.java
+++ app/src/main/java/com/example/buildcollab/fragments/GroupFragment.java
@@ -10,11 +10,17 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.buildcollab.R;
 import com.example.buildcollab.activity.NewGroupActivity;
 
 public class GroupFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -27,6 +33,17 @@ public class GroupFragment extends Fragment {
                 startActivity(intent);
             }
         });
+
+        mRecyclerView = InputFragmentView.findViewById(R.id.reclycleview);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(InputFragmentView.getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+
+        //Falta a leitura dos dados
+        //   mAdapter = new MyAdapter(LoadData);
+        mRecyclerView.setAdapter(mAdapter);
         return InputFragmentView;
     }
 }
